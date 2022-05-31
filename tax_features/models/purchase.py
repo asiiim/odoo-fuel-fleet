1bb590eb9f95c240e0f965d78fac42e74c86a040
Aashim Bajracharya <ashimbazracharya@gmail.com>
2022-05-31 16:04:28 +0545

    [FIX] Fix the filtration of tax population

--- tax_features/models/purchase.py
+++ tax_features/models/purchase.py
@@ -37,7 +37,7 @@ class PurchaseOrder(models.Model):
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    
+
     @api.onchange('product_id')
     def onchange_product_id(self):
         
@@ -57,33 +57,19 @@ class PurchaseOrderLine(models.Model):
 
             # Add those taxes in the list
             taxes_list = []
+            
             for tax in taxes:
-                taxes_list.append(tax)
-
-            print('taxes>>>>>>>>>>>>>>>>>', taxes, 
-            '\ncateg>>>>>>>>>>>>>>>', categ)
-
-            # Filter out the tax recs on the basis of product category
-            for i, tax in enumerate(taxes_list):
-                print('tax product categssss>>>>>>>', tax.product_categs_id)
                 if categ in tax.product_categs_id:
-                    continue
-                else:
-                    taxes_list.pop(i)
-            print(taxes_list)
+                    taxes_list.append(tax)
+
 
             # Lookup each selected tax rate from the realtime tax rate model
             realtime_tax_rate_recs = self.env['realtime.tax.rate'].search([
                 ('date_time', '<=', self.order_id.lift_datetime)
             ], order='date_time desc')
 
-            print(taxes.mapped('amount'))
-            print(realtime_tax_rate_recs.\
-                        mapped('tax_lines').mapped('tax_id').mapped('name')
-                    )
-            
             if realtime_tax_rate_recs:
-                for i, tax in enumerate(taxes):
+                for i, tax in enumerate(taxes_list):
                     rate_rec = realtime_tax_rate_recs.\
                         mapped('tax_lines').filtered(
                         lambda x: x.tax_id == tax
@@ -91,7 +77,8 @@ class PurchaseOrderLine(models.Model):
 
                     if rate_rec:
                         taxrate = rate_rec.mapped('rate')[0]
-                        taxes[i].write({'amount': taxrate})
-            print(taxes.mapped('amount'))
-            self.taxes_id = taxes
+                        taxes_list[i].write({'amount': taxrate})
+            
+            for tax in taxes_list:
+                self.taxes_id += tax
         return result
