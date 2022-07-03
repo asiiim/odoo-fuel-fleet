5be07966c7ba7d70cab7a3fadf2ebfd003872d8c
Aashim Bajracharya <ashimbazracharya@gmail.com>
2022-07-03 18:04:46 +0545

    feat: add module to update the selected picking type in sale order to delivery order

---
+++ sale_stock_picking/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sales Stock Picking",
+
+    'summary': """
+        Have stock picking type selection in Sales Order.
+    """,
+    "description": """
+        Features:
+        \n- Add option to select stock picking type in sale order and reflect that picking type in 
+            related delivery orders picking type.
+    """,
+    'sequence': '1',
+    'author': "Aashim Bajracharya",
+    'website': "https://www.linkedin.com/in/aashim-bajracharya-860406181/",
+    'category': 'Sale',
+    'version': '15.0.0.0.1',
+
+    'depends': [
+        'sale_stock'
+    ],
+
+    'data': [
+        'views/sale.xml',
+    ],
+
+    'application': True
+}
---
+++ sale_stock_picking/models/sale.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime, timedelta
+from odoo import api, fields, models, _
+from collections import defaultdict
+from odoo.exceptions import UserError
+
+import json
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    READONLY_STATES = {
+        'sale': [('readonly', True)],
+        'done': [('readonly', True)],
+        'cancel': [('readonly', True)],
+    }
+
+    @api.model
+    def _default_picking_type(self):
+        return self._get_picking_type(self.env.context.get('company_id') or self.env.company.id)
+
+    
+    @api.model
+    def _get_picking_type(self, company_id):
+        picking_type = self.env['stock.picking.type'].search([('code', '=', 'outgoing'), ('warehouse_id.company_id', '=', company_id)])
+        if not picking_type:
+            picking_type = self.env['stock.picking.type'].search([('code', '=', 'outgoing'), ('warehouse_id', '=', False)])
+        return picking_type[:1]
+
+    
+    picking_type_id = fields.Many2one('stock.picking.type', 
+        'Pick From', states=READONLY_STATES, required=True, 
+        default=_default_picking_type, 
+        domain="['|', ('warehouse_id', '=', False), ('warehouse_id.company_id', '=', company_id)]",
+        help="This will determine operation type of outgoing shipment (delivery)")
+
+
+    # Set data from sale to stock picking
+    def _get_action_view_picking(self, pickings):
+        result = super(SaleOrder, self)._get_action_view_picking(pickings)
+
+        if pickings:
+            for picking in pickings:
+                if picking.state in ('draft', 'waiting', 'confirmed', 'confirmed'):
+                    picking.update({'picking_type_id': self.picking_type_id.id})
+        return result
+
