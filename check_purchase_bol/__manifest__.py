58b287821f5b49c1a6b4d44542d63bc1853284ac
Aashim B <ashimbazracharya@gmail.com>
2022-07-07 10:25:36 +0545

    clean: fix code formats

--- check_purchase_bol/__manifest__.py
+++ check_purchase_bol/__manifest__.py
@@ -1,26 +1,19 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Purchase Orders By Bill of Lading",
-
-    'summary': """
+    "name": "Purchase Orders By Bill of Lading",
+    "summary": """
         Check and view purchase order by entered Bill of Lading reference.
     """,
-    
-    'sequence': '1',
-    'author': "Aashim Bajracharya",
-    'website': "https://www.linkedin.com/in/aashim-bajracharya-860406181/",
-    'category': 'Purchase',
-    'version': '14.0.0.0.1',
-
-    'depends': [
-        'purchase_features'
-    ],
-
-    'data': [
+    "sequence": "1",
+    "author": "Aashim Bajracharya",
+    "website": "https://www.linkedin.com/in/aashim-bajracharya-860406181/",
+    "category": "Purchase",
+    "version": "14.0.0.0.1",
+    "depends": ["purchase_features"],
+    "data": [
         # 'security/security.xml',
-        'security/ir.model.access.csv',
-        'wizards/purchase_bol.xml'
+        "security/ir.model.access.csv",
+        "wizards/purchase_bol.xml",
     ],
-
-    'application': True
+    "application": True,
 }
--- check_purchase_bol/wizards/purchase_bol.py
+++ check_purchase_bol/wizards/purchase_bol.py
@@ -6,45 +6,44 @@ class PurchaseBolWizard(models.TransientModel):
     _name = "purchase.bol.wizard"
     _description = "Wizard To Search Purchase by BOL"
 
-    
-    name = fields.Char('BOL#', help='Bill of Lading')
-
+    name = fields.Char("BOL#", help="Bill of Lading")
 
     def search_purchase_orders(self):
-        '''
-            - get purchase orderline records
-            - search by bill of lading such that BOL value is in both upper and lower
-        '''
+        """
+        - get purchase orderline records
+        - search by bill of lading such that BOL value is in both upper and lower
+        """
 
-        purchase_orderline_recs = self.env['purchase.order.line'].search([])
+        purchase_orderline_recs = self.env["purchase.order.line"].search([])
         filtered_recs = purchase_orderline_recs.filtered(
-            lambda orderline: str(orderline.bol_ref).casefold() == str(self.name).casefold()).mapped('order_id')
-        print('Info::: Searched Purchase Orders: ', filtered_recs.mapped('name'))
+            lambda orderline: str(orderline.bol_ref).casefold()
+            == str(self.name).casefold()
+        ).mapped("order_id")
+        print("Info::: Searched Purchase Orders: ", filtered_recs.mapped("name"))
         return filtered_recs
 
-    
     def action_view_purchases(self):
         purchases = self.search_purchase_orders()
-        
-        action = self.env['ir.actions.act_window']\
-            ._for_xml_id('purchase.purchase_form_action')
-        
+
+        action = self.env["ir.actions.act_window"]._for_xml_id(
+            "purchase.purchase_form_action"
+        )
+
         if len(purchases) > 1:
-            action['domain'] = [('id', 'in', purchases.ids)]
-        
+            action["domain"] = [("id", "in", purchases.ids)]
+
         elif len(purchases) == 1:
-            res = self.env.ref('purchase.purchase_order_form', False)
-            form_view = [(res and res.id or False, 'form')]
+            res = self.env.ref("purchase.purchase_order_form", False)
+            form_view = [(res and res.id or False, "form")]
 
-            if 'views' in action:
-                action['views'] = form_view \
-                    + [(state, view) for state, view in action['views'] if view != 'form']
+            if "views" in action:
+                action["views"] = form_view + [
+                    (state, view) for state, view in action["views"] if view != "form"
+                ]
             else:
-                action['views'] = form_view
-            action['res_id'] = purchases.id
+                action["views"] = form_view
+            action["res_id"] = purchases.id
         else:
-            return {'type': 'ir.actions.act_window_close'}
-        
+            return {"type": "ir.actions.act_window_close"}
+
         return action
-
-
