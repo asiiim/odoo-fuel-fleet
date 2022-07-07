90b869c44f175708e97d8a8e41db4df767a1da7c
Aashim B <ashimbazracharya@gmail.com>
2022-07-07 10:53:47 +0545

    refactor: clean up and reformat codes

--- stock_features/__manifest__.py
+++ stock_features/__manifest__.py
@@ -1,28 +1,24 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Stock Features",
-
-    'summary': """
+    "name": "Stock Features",
+    "summary": """
         Add features in stock related documents.
     """,
     "description": """
         Features:
         \n- Add bill of lading reference in stock picking and stock moves records.
     """,
-    'sequence': '1',
-    'author': "Aashim Bajracharya",
-    'website': "https://www.linkedin.com/in/aashim-bajracharya-860406181/",
-    'category': 'Stock',
-    'version': '14.0.0.0.1',
-
-    'depends': [
-        'purchase_features',
+    "sequence": "1",
+    "author": "Aashim Bajracharya",
+    "website": "https://www.linkedin.com/in/aashim-bajracharya-860406181/",
+    "category": "Stock",
+    "version": "14.0.0.0.1",
+    "depends": [
+        "purchase_features",
     ],
-
-    'data': [
-        'views/stock_picking.xml',
+    "data": [
+        "views/stock_picking.xml",
         # 'views/stock_move_line.xml',
     ],
-
-    'application': True
+    "application": True,
 }
--- stock_features/models/stock_picking.py
+++ stock_features/models/stock_picking.py
@@ -4,30 +4,31 @@ from datetime import datetime, timedelta
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
+
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
-
-    terminal_id = fields.Many2one('fuel.terminal')
-    carrier_id = fields.Many2one('res.partner', domain="[('is_carrier', '=', True)]",
-        help='''Carriers (trucking companies) approved to lift product from the specific 
-            Supplier at the specific Terminal''')
-    driver_id = fields.Many2one('res.partner', domain="[('is_driver', '=', True)]")
+    terminal_id = fields.Many2one("fuel.terminal")
+    carrier_id = fields.Many2one(
+        "res.partner",
+        domain="[('is_carrier', '=', True)]",
+        help="""Carriers (trucking companies) approved to lift product from the specific 
+            Supplier at the specific Terminal""",
+    )
+    driver_id = fields.Many2one("res.partner", domain="[('is_driver', '=', True)]")
     lift_datetime = fields.Datetime(
-        string='Lift Date & Time', 
-        index=True, 
-        copy=False,
-        tracking=True)
-
+        string="Lift Date & Time", index=True, copy=False, tracking=True
+    )
 
     def button_validate(self):
         result = super(
-            StockPicking, 
-            self.with_context(skip_backorder=True, picking_ids_not_to_backorder=self.id))\
-                .button_validate()
+            StockPicking,
+            self.with_context(
+                skip_backorder=True, picking_ids_not_to_backorder=self.id
+            ),
+        ).button_validate()
 
         return result
 
-    
     # Bill of lading
-    bol_ref = fields.Char('BOL#', help='Bill of Lading', copy=False)
+    bol_ref = fields.Char("BOL#", help="Bill of Lading", copy=False)
